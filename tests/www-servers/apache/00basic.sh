ae5d0fbdc6182df5d32d393b813dfd1152c1d0c8
Stefan Kuhn <wuodan@hispeed.ch>
2012-07-21 11:19:20 +0200

    backup

--- tests/www-servers/apache/00basic.sh
+++ tests/www-servers/apache/00basic.sh
@@ -3,6 +3,7 @@
 
 MY_FLAGS=
 CRITICAL_FLAGS='apache2_modules_authz_host apache2_modules_dir apache2_modules_mime'
+DAV_FLAGS=" apache2_modules_dav_fs apache2_modules_dav_lock"
 
 # remove flags, 2^21 combinations is to much
 pkg_flags()
@@ -11,6 +12,10 @@ pkg_flags()
 	FLAGS=""
 	# remove critical
 	for flag in $CRITICAL_FLAGS; do
+		MY_FLAGS=${MY_FLAGS/ $flag / }
+	done
+	# remove dav
+	for flag in $DAV_FLAGS; do
 		MY_FLAGS=${MY_FLAGS/ $flag / }
 	done
 	# remove trailing space
@@ -26,6 +31,12 @@ pkg_flag_combinations()
 		FLAG_COMBINATIONS+=" ${flagsmin/ -$flag/ $flag}"
 		# append critical flags
 		FLAG_COMBINATIONS+=" ${CRITICAL_FLAGS}"
+		# append dav flags
+		if [ "$flag" == 'apache2_modules_dav' ]; then
+			FLAG_COMBINATIONS+=" ${DAV_FLAGS}"
+		else
+			FLAG_COMBINATIONS+=" ${DAV_FLAGS// / -}"
+		fi
 		if [ "$flag" != "$lastflag" ];then
 			FLAG_COMBINATIONS+=$'\n'
 		fi
@@ -35,8 +46,16 @@ pkg_flag_combinations()
 pkg_test()
 {
 	test_atom "/etc/init.d/apache2 restart"
-	test_atom "wget 127.0.0.1"
+	test_atom "wget localhost"
 	if [ "`cat index.html`" != '<html><body><h1>It works!</h1></body></html>' ]; then
 		die "index.html check fails"
+	fi
+}
+
+pkg_test_ssl()
+{
+	test_atom "wget --no-check-certificate  https://localhost"
+	if [ "`cat index.html`" != '<html><body><h1>It works!</h1></body></html>' ]; then
+		die "SSL index.html check fails"
 	fi
 }
