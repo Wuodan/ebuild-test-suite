f72164c0c57d217e1f370f3f2c708c7440dd9ddf
Stefan Kuhn <wuodan@hispeed.ch>
2012-07-23 13:47:27 +0200

    backup

--- tests/www-servers/apache/00basic.sh
+++ tests/www-servers/apache/00basic.sh
@@ -12,9 +12,7 @@ IUSE_MPMS_THREAD="event worker"
 # itk : configure: error: MPM itk is not supported on this platform. (amd64)
 # peruser : configure: error: MPM peruser is not supported on this platform. (amd64)
 # proxy_scgi: Syntax error on line 145 of /etc/apache2/httpd.conf: Cannot load /usr/lib64/apache2/modules/mod_proxy_scgi.so into server: /usr/lib64/apache2/modules/mod_proxy_scgi.so: undefined symbol: ap_proxy_release_connection
-# static: AH00526: Syntax error on line 67 of /etc/apache2/httpd.conf: Invalid command 'User', perhaps misspelled or defined by a module not included in the server configuration. Plus hangs when max flags are activated.
-# reminder => activate static test below once fixed
-DISABLED_FLAGS=" apache2_mpms_itk apache2_mpms_peruser apache2_modules_proxy_scgi static"
+DISABLED_FLAGS=" apache2_mpms_itk apache2_mpms_peruser apache2_modules_proxy_scgi"
 
 DOC_FLAGS=" alias negotiation setenvif"
 SSL_FLAGS=" socache_shmcb"
@@ -23,12 +21,17 @@ FILTER_FLAGS=" deflate ext_filter substitute"
 CACHE_FLAGS=" cache_disk file_cache"
 LOG_CONFIG_FLAGS=" log_forensic logio"
 MIME_FLAGS=" mime_magic"
-PROXY_FLAGS=" proxy_ajp proxy_balancer proxy_connect proxy_ftp proxy_http proxy_scgi"
+PROXY_BALANCER_FLAGS=" lbmethod_byrequests lbmethod_bytraffic lbmethod_bybusyness lbmethod_heartbeat"
+PROXY_FLAGS="${PROXY_BALANCER_FLAGS} proxy_ajp proxy_balancer proxy_connect proxy_ftp proxy_http proxy_scgi"
+SLOTMEM_SHM_FLAGS=" lbmethod_byrequests"
+# proxy_balancer needs at least one LBMETHOD
+LBMETHOD_BYTRAFFIC_FLAGS=" proxy_balancer"
 
 # remove flags, 2^21 combinations is to much
 pkg_flags()
 {
-	MY_FLAGS=" ${FLAGS} "
+	# remove static
+	MY_FLAGS=" ${FLAGS/ static / }"
 	FLAGS=""
 	# remove critical
 	for flag in $CRITICAL_FLAGS; do
@@ -49,7 +52,6 @@ pkg_flags()
 pkg_flag_combinations()
 {
 	local flagsmin=${MY_FLAGS// / -}
-	local lastflag="${MY_FLAGS##* }"
 	local line=
 
 	# append negated disabled flags to critical
@@ -58,25 +60,149 @@ pkg_flag_combinations()
 	# one line with minimum active flags
 	line=" ${flagsmin}"
 	# append critical flags
-	line+=" ${CRITICAL_FLAGS}"
+	line+=" ${CRITICAL_FLAGS} -static"
 	FLAG_COMBINATIONS+="$line"
 	FLAG_COMBINATIONS+=$'\n'
 
-	# one line per MPMS with maximum active flags
 	# once with static flag and once without
 	local static=
-	# reminder => static<=2 once static is fixed
-	for (( static=0; static<=1; static++)); do
+	for (( static=0; static<2; static++)); do
+		
+
+		# add a single line for each flag
+		for flag in $MY_FLAGS; do
+			line=" ${flagsmin/ -$flag/ $flag}"
+			# append critical flags
+			line+=" ${CRITICAL_FLAGS}"
+
+			# thread MPMS
+			for mflag in $IUSE_MPMS_THREAD; do
+				if [ "$flag" == "apache2_mpms_$mflag" ]; then
+					line=${line/ -threads / threads }
+					break
+				fi
+			done
+
+			# doc depends on these modules
+			if [ $flag == doc ]; then
+				for mflag in $DOC_FLAGS; do
+					line=${line/ -apache2_modules_$mflag / apache2_modules_$mflag }
+				done
+			fi
+
+			# ssl depends on these modules
+			if [ $flag == ssl ]; then
+				for mflag in $SSL_FLAGS; do
+					line=${line/ -apache2_modules_$mflag / apache2_modules_$mflag }
+				done
+			fi
+
+			# handle dav flags
+			local dflag=dav
+			for d in $DAV_FLAGS; do
+				if [ "apache2_modules_$d" == $flag ]; then
+					line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
+					line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
+				fi
+			done
+
+			# handle filter flags
+			local dflag=filter
+			for d in $FILTER_FLAGS; do
+				if [ "apache2_modules_$d" == $flag ]; then
+					line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
+					line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
+				fi
+			done
+
+			# handle cache flags
+			local dflag=cache
+			for d in $CACHE_FLAGS; do
+				if [ "apache2_modules_$d" == $flag ]; then
+					line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
+					line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
+				fi
+			done
+
+			# handle log_config flags
+			local dflag=log_config
+			for d in $LOG_CONFIG_FLAGS; do
+				if [ "apache2_modules_$d" == $flag ]; then
+					line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
+					line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
+				fi
+			done
+
+			# handle mime flags
+			local dflag=mime
+			for d in $MIME_FLAGS; do
+				if [ "apache2_modules_$d" == $flag ]; then
+					line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
+					line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
+				fi
+			done
+
+			# handle proxy flags
+			local dflag=proxy
+			for d in $PROXY_FLAGS; do
+				if [ "apache2_modules_$d" == $flag ]; then
+					line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
+					line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
+				fi
+			done
+
+			# handle proxy_balancer flags
+			local dflag=proxy_balancer
+			for d in $PROXY_BALANCER_FLAGS; do
+				if [ "apache2_modules_$d" == $flag ]; then
+					line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
+					line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
+				fi
+			done
+
+			# handle slotmem_shm flags
+			local dflag=slotmem_shm
+			for d in $SLOTMEM_SHM_FLAGS; do
+				if [ "apache2_modules_$d" == $flag ]; then
+					line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
+					line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
+				fi
+			done
+
+			# handle lbmethod_bytraffic flags
+			local dflag=lbmethod_bytraffic
+			for d in $SLOTMEM_SHM_FLAGS; do
+				if [ "apache2_modules_$d" == $flag ]; then
+					line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
+					line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
+				fi
+			done
+
+			# toggle static flag
+			if [ $static -eq 0 ]; then
+				line+=" static"
+			else
+				line+=" -static"
+			fi
+
+			FLAG_COMBINATIONS+="$line"
+			FLAG_COMBINATIONS+=$'\n'
+		done
+
+		# one line per MPMS with maximum active flags
+		local lastflag="apache2_mpms_${IUSE_MPMS_THREAD##* }"
 		for flag in $IUSE_MPMS_FORK $IUSE_MPMS_THREAD; do
-			line=" ${MY_FLAGS/ static / }"
+			line=" ${MY_FLAGS}"
 			# append critical flags
 			line+=" ${CRITICAL_FLAGS} "
+
 			# disable all other MPMS
 			for mflag in $IUSE_MPMS_FORK $IUSE_MPMS_THREAD; do
 				if [ "$flag" != "$mflag" ]; then
 					line=${line/ apache2_mpms_$mflag / -apache2_mpms_$mflag }
 				fi
 			done
+			
 			# no thread MPMS
 			for mflag in $IUSE_MPMS_FORK; do
 				if [ "$flag" == "$mflag" ]; then
@@ -84,102 +210,21 @@ pkg_flag_combinations()
 					break
 				fi
 			done
-			# reminder => enable once static is fixed
+		
 			# toggle static flag
-			# if [ $static -eq 0 ]; then
-				# line+=" -static"
-			# else
-				# line+=" static"
-			# fi
+			if [ $static -eq 0 ]; then
+				line+=" static"
+			else
+				line+=" -static"
+			fi
 			FLAG_COMBINATIONS+="$line"
-			FLAG_COMBINATIONS+=$'\n'
-		done
-	done
-
-	# add a single line for each flag
-	for flag in $MY_FLAGS; do
-		line=" ${flagsmin/ -$flag/ $flag}"
-		# append critical flags
-		line+=" ${CRITICAL_FLAGS}"
-
-		# thread MPMS
-		for mflag in $IUSE_MPMS_THREAD; do
-			if [ "$flag" == "apache2_mpms_$mflag" ]; then
-				line=${line/ -threads / threads }
-				break
+			if [ "$flag" != "$lastflag" ]; then
+				echo "Adding enter ..."
+				FLAG_COMBINATIONS+=$'\n'
 			fi
 		done
-
-		# doc depends on these modules
-		if [ $flag == doc ]; then
-			for mflag in $DOC_FLAGS; do
-				line=${line/ -apache2_modules_$mflag / apache2_modules_$mflag }
-			done
-		fi
-
-		# ssl depends on these modules
-		if [ $flag == ssl ]; then
-			for mflag in $SSL_FLAGS; do
-				line=${line/ -apache2_modules_$mflag / apache2_modules_$mflag }
-			done
-		fi
-
-		# handle dav flags
-		local dflag=dav
-		for d in $DAV_FLAGS; do
-			if [ "apache2_modules_$d" == $flag ]; then
-				line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
-				line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
-			fi
-		done
-
-		# handle filter flags
-		local dflag=filter
-		for d in $FILTER_FLAGS; do
-			if [ "apache2_modules_$d" == $flag ]; then
-				line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
-				line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
-			fi
-		done
-
-		# handle cache flags
-		local dflag=cache
-		for d in $CACHE_FLAGS; do
-			if [ "apache2_modules_$d" == $flag ]; then
-				line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
-				line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
-			fi
-		done
-
-		# handle log_config flags
-		local dflag=log_config
-		for d in $LOG_CONFIG_FLAGS; do
-			if [ "apache2_modules_$d" == $flag ]; then
-				line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
-				line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
-			fi
-		done
-
-		# handle mime flags
-		local dflag=mime
-		for d in $MIME_FLAGS; do
-			if [ "apache2_modules_$d" == $flag ]; then
-				line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
-				line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
-			fi
-		done
-
-		# handle proxy flags
-		local dflag=proxy
-		for d in $PROXY_FLAGS; do
-			if [ "apache2_modules_$d" == $flag ]; then
-				line=" ${line/ -apache2_modules_$dflag / apache2_modules_$dflag }"
-				line=" ${line/ -apache2_modules_$dflag$/ apache2_modules_$dflag}"
-			fi
-		done
-
-		FLAG_COMBINATIONS+="$line"
-		if [ "$flag" != "$lastflag" ]; then
+		
+		if [ $static -eq 0 ]; then
 			FLAG_COMBINATIONS+=$'\n'
 		fi
 	done
